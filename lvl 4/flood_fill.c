a247f0523e2d2cd44b12c832b7cc595c8a999e35
Hushy <99977761+Hushhhy@users.noreply.github.com>
2024-09-16 17:57:59 +0200

    Update flood_fill.c

--- lvl 4/flood_fill.c
+++ lvl 4/flood_fill.c
@@ -6,7 +6,7 @@
 
 void    fill(char **tab, t_point size, t_point cur, char to_fill)
 {
-    if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
+    if (cur.y < 0 || cur.x < 0 || cur.y >= size.y || cur.x >= size.x || tab[cur.y][cur.x] != to_fill)
         return ;
     tab[cur.y][cur.x] = 'F';
     fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
