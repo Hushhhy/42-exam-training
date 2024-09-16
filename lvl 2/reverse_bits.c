5f79af2b74ff2c3003a915dce6020ac01d31d22a
Hushy <99977761+Hushhhy@users.noreply.github.com>
2024-09-16 17:23:09 +0200

    fix reverse_bits error

--- lvl 2/reverse_bits.c
+++ lvl 2/reverse_bits.c
@@ -1,12 +1,11 @@
 unsigned char reverse_bits(unsigned char octet)
 {
-    return (((octet >> 8) << 0) |
-            ((octet >> 7) << 1) |
-            ((octet >> 6) << 2) |
-            ((octet >> 5) << 3) |
-            ((octet >> 4) << 4) |
-            ((octet >> 3) << 5) |
-            ((octet >> 2) << 6) |
-            ((octet >> 1) << 7) |
-            ((octet >> 0) << 8));
+    return (((octet >> 7) << 0) |
+            ((octet >> 6) << 1) |
+            ((octet >> 5) << 2) |
+            ((octet >> 4) << 3) |
+            ((octet >> 3) << 4) |
+            ((octet >> 2) << 5) |
+            ((octet >> 1) << 6) |
+            ((octet >> 0) << 7));
 }
